e7f3a55d2910d6c3c1244e2a68bb8f077d8978c3
Jouhar <jouharvaramangalath786@gmail.com>
2024-08-27 15:51:49 +0530

    Your commit message

---
+++ src/contexts/LoginContext.jsx
@@ -0,0 +1,13 @@
+import { createContext, useState } from 'react'
+export const LoginContext = createContext();
+
+export const LoginStatusProvider = ({children}) => {
+  const [existedUser, setExistedUser] = useState([]);
+
+  return (
+    <LoginContext.Provider value={{existedUser, setExistedUser}} >
+        {children}
+    </LoginContext.Provider>
+  )
+}
+
